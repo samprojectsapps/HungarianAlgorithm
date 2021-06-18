3e4f32e8eab3a3c0d20fd92fbb6915bc8286b440
samprojectsapps <63936415+samprojectsapps@users.noreply.github.com>
2021-06-17 22:00:26 -0400

    Update HungarianAlgorithm.swift

--- HungarianAlgorithm.swift
+++ HungarianAlgorithm.swift
@@ -30,174 +30,174 @@ struct Line: Hashable, Equatable, Comparable {
     }
 }
 
-    func modifiedGetMinLines(_ matrix: [[Int]]) -> Set<Line> { // O(N^4)
-        
-        // Using the algorithm found here - https://www.youtube.com/watch?v=rrfFTdO2Z7I
-        func drawLinesWhileIsolatedZerosExist(_ matrix: inout [[Int]]) -> Set<Line> { // O(N^3)
-            
-            let N = matrix.count
-            
-            var lines: Set<Line> = []
-            var unprocessedTableChange = true
-            while unprocessedTableChange { // While loop occurs 2N-1 times max!...each time a line in a matrix must be crossed out to continue
-                unprocessedTableChange = false
-                
-                for i in 0..<N { // rows
-                    var zeroCount = 0
-                    var columnOfLastZero = -1
-                    for j in 0..<N {
-                        if matrix[i][j] == 0 {
-                            zeroCount += 1
-                            columnOfLastZero = j
-                        }
-                    }
-                    
-                    if zeroCount == 1 {
-                        unprocessedTableChange = true
-                        
-                        var selectedCol = Line(columnOfLastZero, .VERTICAL)
-                        for i in 0..<N {
-                            if matrix[i][columnOfLastZero] == 0 {
-                                selectedCol.coord.insert(i)
-                            }
-                            matrix[i][columnOfLastZero] = -1 // Cross line out
-                        }
-                        lines.insert(selectedCol)
-                    }
-                }
-                
-                for i in 0..<N { // columns
-                    var zeroCount = 0
-                    var rowOfLastZero = -1
-                    for j in 0..<N {
-                        if matrix[j][i] == 0 {
-                            zeroCount += 1
-                            rowOfLastZero = j
-                        }
-                    }
-                    
-                    if zeroCount == 1 {
-                        unprocessedTableChange = true
-                        
-                        var selectedRow = Line(rowOfLastZero, .HORIZONTAL)
-                        for i in 0..<N {
-                            if matrix[rowOfLastZero][i] == 0 {
-                                selectedRow.coord.insert(i)
-                            }
-                            matrix[rowOfLastZero][i] = -1 // Cross line out
-                        }
-                        lines.insert(selectedRow)
-                    }
-                }
-            }
-            
-            return lines
-        }
-        
-        func zerosToProcessExist(_ array: [Int]) -> Bool { // O(N)
-            for e in array {
-                if e > 0 { return true }
-            }
-            return false
-        }
-        
-        var matrix = matrix
+func modifiedGetMinLines(_ matrix: [[Int]]) -> Set<Line> { // O(N^4)
+
+    // Using the algorithm found here - https://www.youtube.com/watch?v=rrfFTdO2Z7I
+    func drawLinesWhileIsolatedZerosExist(_ matrix: inout [[Int]]) -> Set<Line> { // O(N^3)
+
         let N = matrix.count
-        var lines: Set<Line> = drawLinesWhileIsolatedZerosExist(&matrix) // O(N^3)
-        var zerosPerRow = Array(repeating: 0, count: N)
-        var zerosPerCol = Array(repeating: 0, count: N)
 
-        for i in 0..<N { // O(N^2)
-            for j in 0..<N {
-                if matrix[i][j] == 0 {
-                    zerosPerRow[i] += 1
-                    zerosPerCol[j] += 1
+        var lines: Set<Line> = []
+        var unprocessedTableChange = true
+        while unprocessedTableChange { // While loop occurs 2N-1 times max!...each time a line in a matrix must be crossed out to continue
+            unprocessedTableChange = false
+
+            for i in 0..<N { // rows
+                var zeroCount = 0
+                var columnOfLastZero = -1
+                for j in 0..<N {
+                    if matrix[i][j] == 0 {
+                        zeroCount += 1
+                        columnOfLastZero = j
+                    }
+                }
+
+                if zeroCount == 1 {
+                    unprocessedTableChange = true
+
+                    var selectedCol = Line(columnOfLastZero, .VERTICAL)
+                    for i in 0..<N {
+                        if matrix[i][columnOfLastZero] == 0 {
+                            selectedCol.coord.insert(i)
+                        }
+                        matrix[i][columnOfLastZero] = -1 // Cross line out
+                    }
+                    lines.insert(selectedCol)
+                }
+            }
+
+            for i in 0..<N { // columns
+                var zeroCount = 0
+                var rowOfLastZero = -1
+                for j in 0..<N {
+                    if matrix[j][i] == 0 {
+                        zeroCount += 1
+                        rowOfLastZero = j
+                    }
+                }
+
+                if zeroCount == 1 {
+                    unprocessedTableChange = true
+
+                    var selectedRow = Line(rowOfLastZero, .HORIZONTAL)
+                    for i in 0..<N {
+                        if matrix[rowOfLastZero][i] == 0 {
+                            selectedRow.coord.insert(i)
+                        }
+                        matrix[rowOfLastZero][i] = -1 // Cross line out
+                    }
+                    lines.insert(selectedRow)
                 }
             }
         }
 
-        while zerosToProcessExist(zerosPerRow) || zerosToProcessExist(zerosPerCol) { // While loop occurs 2N-1 times max!...each time a line in a matrix must be crossed out to continue
-            
-            var max = 0
-            var lineWithMostZeros: Line?
-            
-            var linesWithMaxZeros: Set<Line> = []
-            for i in 0..<N { // O(N)
-                if zerosPerRow[i] > max {
-                    linesWithMaxZeros = []
-                    linesWithMaxZeros.insert(Line(i, LineType.HORIZONTAL))
-                    max = zerosPerRow[i]
-                } else if zerosPerRow[i] == max && max > 0 {
-                    linesWithMaxZeros.insert(Line(i, LineType.HORIZONTAL))
-                }
-                
-                if zerosPerCol[i] > max {
-                    linesWithMaxZeros = []
-                    linesWithMaxZeros.insert(Line(i, LineType.VERTICAL))
-                    max = zerosPerCol[i]
-                } else if zerosPerCol[i] == max && max > 0 {
-                    linesWithMaxZeros.insert(Line(i, LineType.VERTICAL))
-                }
-            }
-
-            if linesWithMaxZeros.count == 1 {
-                lineWithMostZeros = linesWithMaxZeros.first
-            } else {
-                var minScore = Int.max
-                var minScoreLine: Line?
-                for l in linesWithMaxZeros {
-                    var score = 0
-                    if l.isHorizontal() {
-                        for j in 0..<N {
-                            if matrix[l.lineIndex][j] == 0 {
-                                for k in 0..<N {
-                                    if matrix[k][j] == 0 { score += 1 }
-                                }
-                            }
-                        }
-                    } else {
-                        for j in 0..<N {
-                            if matrix[j][l.lineIndex] == 0 {
-                                for k in 0..<N {
-                                    if matrix[j][k] == 0 { score += 1 }
-                                }
-                            }
-                        }
-                    }
-                    if score < minScore {
-                        minScore = score
-                        minScoreLine = l
-                    }
-                }
-                lineWithMostZeros = minScoreLine
-            }
-            
-            let index = lineWithMostZeros!.lineIndex
-            var temp: Set<Int> = []
-            if lineWithMostZeros!.isHorizontal() { // O(N)
-                zerosPerRow[index] = 0
-                for j in 0..<N {
-                    if matrix[index][j] == 0 {
-                        zerosPerCol[j] -= 1
-                        temp.insert(j)
-                    }
-                    matrix[index][j] = -1
-                }
-            } else {
-                zerosPerCol[index] = 0
-                for j in 0..<N {
-                    if matrix[j][index] == 0 {
-                        zerosPerRow[j] -= 1
-                        temp.insert(j)
-                    }
-                    matrix[j][index] = -1
-                }
-            }
-            lineWithMostZeros!.coord = temp
-            lines.insert(lineWithMostZeros!)
-        }
         return lines
     }
+
+    func zerosToProcessExist(_ array: [Int]) -> Bool { // O(N)
+        for e in array {
+            if e > 0 { return true }
+        }
+        return false
+    }
+
+    var matrix = matrix
+    let N = matrix.count
+    var lines: Set<Line> = drawLinesWhileIsolatedZerosExist(&matrix) // O(N^3)
+    var zerosPerRow = Array(repeating: 0, count: N)
+    var zerosPerCol = Array(repeating: 0, count: N)
+
+    for i in 0..<N { // O(N^2)
+        for j in 0..<N {
+            if matrix[i][j] == 0 {
+                zerosPerRow[i] += 1
+                zerosPerCol[j] += 1
+            }
+        }
+    }
+
+    while zerosToProcessExist(zerosPerRow) || zerosToProcessExist(zerosPerCol) { // While loop occurs 2N-1 times max!...each time a line in a matrix must be crossed out to continue
+
+        var max = 0
+        var lineWithMostZeros: Line?
+
+        var linesWithMaxZeros: Set<Line> = []
+        for i in 0..<N { // O(N)
+            if zerosPerRow[i] > max {
+                linesWithMaxZeros = []
+                linesWithMaxZeros.insert(Line(i, LineType.HORIZONTAL))
+                max = zerosPerRow[i]
+            } else if zerosPerRow[i] == max && max > 0 {
+                linesWithMaxZeros.insert(Line(i, LineType.HORIZONTAL))
+            }
+
+            if zerosPerCol[i] > max {
+                linesWithMaxZeros = []
+                linesWithMaxZeros.insert(Line(i, LineType.VERTICAL))
+                max = zerosPerCol[i]
+            } else if zerosPerCol[i] == max && max > 0 {
+                linesWithMaxZeros.insert(Line(i, LineType.VERTICAL))
+            }
+        }
+
+        if linesWithMaxZeros.count == 1 {
+            lineWithMostZeros = linesWithMaxZeros.first
+        } else {
+            var minScore = Int.max
+            var minScoreLine: Line?
+            for l in linesWithMaxZeros {
+                var score = 0
+                if l.isHorizontal() {
+                    for j in 0..<N {
+                        if matrix[l.lineIndex][j] == 0 {
+                            for k in 0..<N {
+                                if matrix[k][j] == 0 { score += 1 }
+                            }
+                        }
+                    }
+                } else {
+                    for j in 0..<N {
+                        if matrix[j][l.lineIndex] == 0 {
+                            for k in 0..<N {
+                                if matrix[j][k] == 0 { score += 1 }
+                            }
+                        }
+                    }
+                }
+                if score < minScore {
+                    minScore = score
+                    minScoreLine = l
+                }
+            }
+            lineWithMostZeros = minScoreLine
+        }
+
+        let index = lineWithMostZeros!.lineIndex
+        var temp: Set<Int> = []
+        if lineWithMostZeros!.isHorizontal() { // O(N)
+            zerosPerRow[index] = 0
+            for j in 0..<N {
+                if matrix[index][j] == 0 {
+                    zerosPerCol[j] -= 1
+                    temp.insert(j)
+                }
+                matrix[index][j] = -1
+            }
+        } else {
+            zerosPerCol[index] = 0
+            for j in 0..<N {
+                if matrix[j][index] == 0 {
+                    zerosPerRow[j] -= 1
+                    temp.insert(j)
+                }
+                matrix[j][index] = -1
+            }
+        }
+        lineWithMostZeros!.coord = temp
+        lines.insert(lineWithMostZeros!)
+    }
+    return lines
+}
 
 func hungarianAlgorithm( _ table: [[Int]]) -> Int {
     var tempTable = table
